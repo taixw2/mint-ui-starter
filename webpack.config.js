ce950187199db7a5ad4ec642465f2d78b67f8fc0
iqqq <254225961@qq.com>
2017-03-14 10:57:43 +0800

    修改webpack配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,73 +1,111 @@
 const path = require('path')
 const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: './src/main.js',
+  entry: {
+    build: './src/main.js',
+  },
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
-    filename: 'build.js',
+    filename: 'js/[name].js?q=[hash]',
+  },
+  resolve: {
+    extensions: ['.js', '.vue', '.css'],
+    alias: {
+      components: './src/components',
+      view: './src/view',
+      assets: './src/assets',
+    },
   },
   module: {
-    rules: [{
+    loaders: [{
       test: /\.vue$/,
       loader: 'vue-loader',
       options: {
-        // vue-loader options go here
+        loaders: {
+          css: ExtractTextPlugin.extract({
+            fallback: 'vue-style-loader?sourceMap',
+            use: 'css-loader?sourceMap',
+          }),
+        },
+        postcss: [
+          require('autoprefixer')({
+            browsers: ['last 20 versions'],
+          }),
+        ],
       },
-    },
-    {
+    }, {
       test: /\.js$/,
       loader: 'babel-loader',
       exclude: /node_modules/,
-    },
-    {
+    }, {
       test: /\.css$/,
-      loader: 'style-loader!css-loader',
-    },
-    {
+      loader: ExtractTextPlugin.extract({
+        fallback: 'style-loader?sourceMap',
+        use: 'css-loader?sourceMap',
+      }),
+    }, {
       test: /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/,
       loader: 'file-loader',
-    },
-    {
+    }, {
       test: /\.(png|jpe?g|gif|svg)(\?\S*)?$/,
       loader: 'file-loader',
       query: {
-        name: '[name].[ext]?[hash]',
+        name: 'images/[name].[ext]?q=[hash]',
       },
     }, {
-      test: /\.([scss|sass])$/,
-      loader: 'style-loader!css-loader!sass-loader',
+      test: /\.scss$/,
+      loader: 'style-loader!css-loader?sourceMap!sass-loader!postcss-loader',
     }],
   },
-  resolve: {
-    alias: {
-      'vue$': 'vue/dist/vue',
-    },
-  },
-  devServer: {
-    historyApiFallback: true,
-    noInfo: true,
-    proxy: {
-      '/api/*': {
-        target: 'http://localhost',
-        secure: false,
-        pathRewrite: {
-          '^/api': '',
-        },
+  // devServer: {
+  //   historyApiFallback: true,
+  //   noInfo: true,
+  //   proxy: {
+  //     '/api/*': {
+  //       target: 'http://192.168.1.177/cgi-bin/pa/xs',
+  //       secure: false,
+  //       pathRewrite: {
+  //         '^/api': '',
+  //       },
+  //     },
+  //   },
+  // },
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new ExtractTextPlugin({
+      allChunks: true,
+      filename: 'styles.css?q=[hash]',
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks(module) {
+      // any required modules inside node_modules are extracted to vendor
+        return (
+        module.resource &&
+        /\.js$/.test(module.resource) &&
+        module.resource.indexOf(path.join(__dirname, './node_modules')) === 0
+        )
       },
-    },
-  },
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'manifest',
+      chunks: ['vendor'],
+    }),
+  ],
   devtool: '#eval-source-map',
 }
 
 if (process.env.NODE_ENV === 'production') {
   module.exports.devtool = '#source-map'
-  // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: 'production',
+        NODE_ENV: '"production"',
       },
     }),
     new webpack.optimize.UglifyJsPlugin({
@@ -75,8 +113,6 @@ if (process.env.NODE_ENV === 'production') {
         warnings: false,
       },
     }),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true,
-    }),
+    new OptimizeCSSPlugin(),
   ])
 }
